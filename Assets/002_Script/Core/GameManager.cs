cf45fa8112d2577eecae2547d073c02067ea41b2
wpdudH <66068922+wpdudH@users.noreply.github.com>
2025-02-16 02:20:07 +0900

    [ADD] Assets & Initial Script

---
+++ Assets/002_Script/Core/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public Dictionary<string, int> playerTendency = new Dictionary<string, int>()
+    {
+        {"Violent", 0},
+        {"Intelligence", 0},
+        {"Sweet", 0},
+        {"Madness", 0}
+    };
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ApplyTendencyChanges(Dictionary<string, int> tendencyChanges)
+    {
+        foreach (var tendency in tendencyChanges)
+        {
+            if(playerTendency.ContainsKey(tendency.Key))
+            {
+                playerTendency[tendency.Key] += tendency.Value;
+                Debug.Log($"스탯 변경: {tendency.Key} +{tendency.Value} → 현재 값: {playerTendency[tendency.Key]}");
+            }
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/002_Script/Question/Query.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static Query;
+
+[CreateAssetMenu(fileName = "NewQuery", menuName = "Game/QueryData")]
+public class Query : ScriptableObject
+{
+    // 질문 내용
+    [TextArea(2, 5)]
+    public string       questionText;
+
+    // 성향 변화를 나타냄.
+    [System.Serializable]
+    public class TendencyChange
+    {
+        public string   tendencyName;
+        public int      value;
+    }
+
+    // 선택지 + 성향 변화 저장
+    [System.Serializable]
+    public class Choice
+    {
+        public string                choiceText;
+        public List<TendencyChange>  TendencyChanges;
+
+        // 선택에 따른 결과 반환
+        public Dictionary<string, int> GetTendencyChange(int index)
+        {
+            Dictionary<string, int> TendencyDict = new Dictionary<string, int>();
+            foreach (TendencyChange change in TendencyChanges)
+            {
+                TendencyDict[change.tendencyName] = change.value;
+            }
+            return TendencyDict;
+        }
+    }
+
+    public List<Choice> choices = new List<Choice>();
+}
